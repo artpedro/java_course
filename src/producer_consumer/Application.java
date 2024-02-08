10cc8d7b7d12eeeb68fcd683bc3d22f41962c438
Artur Pedro <arturpedromartins@gmail.com>
2024-02-08 12:05:31 -0300

    BlockingQueue comment added

--- src/producer_consumer/Application.java
+++ src/producer_consumer/Application.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+        // Obs: if we used an ArrayBlockingQueue here, we wouldn't need to hard code the add to last and get the first
+        //      and it would automatically put threads to wait when the queue gets full or empty;
+        //      BlockingQueue<Integer> list = new ArrayBlockingQueue<>();
 
         Thread t1 = new Thread(new Producer(list));
         Thread t2 = new Thread(new Consumer(list));
